4bc6cf47d32ad8c2748b57f75a4305f24e25dd49
maharjanaman191-cell <maharjanaman191@gmail.com>
2026-02-03 18:47:50 +0930

    adding comments

--- image.py
+++ image.py
@@ -34,3 +34,31 @@ class Image:
        # Increase or decrease brightness
     def adjust_brightness(self, value):
         return cv2.convertScaleAbs(self.image, alpha=1, beta=value)
+   
+    # Change image contrast
+    def adjust_contrast(self, value):
+        alpha = value / 50
+        return cv2.convertScaleAbs(self.image, alpha=alpha, beta=0)
+
+# Rotate image based on selected angle
+    def rotate(self, angle):
+        if angle == 90:
+            return cv2.rotate(self.image, cv2.ROTATE_90_CLOCKWISE)
+        elif angle == 180:
+            return cv2.rotate(self.image, cv2.ROTATE_180)
+        elif angle == 270:
+            return cv2.rotate(self.image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+#  Flip image left-right or up-down
+    def flip(self, mode):
+        if mode == "Horizontal":
+            return cv2.flip(self.image, 1)
+        elif mode == "Vertical":
+            return cv2.flip(self.image, 0)
+
+# Resize image using scale value
+    def resize(self, scale):
+        width = int(self.image.shape[1] * scale)
+        height = int(self.image.shape[0] * scale)
+        return cv2.resize(self.image, (width, height))   
+    
