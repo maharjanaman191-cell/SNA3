3c7c2ef88a378f83506f2dc6d66ceae00b04778d
rishichaulagain27-sys <rishi.chaulagain27@gmail.com>
2026-02-04 22:48:37 +0930

    adding more code  gui.py

--- gui.py
+++ gui.py
@@ -9,4 +9,25 @@ class ImageEditorGUI:
         self.controller = controller
         self.root.title("HIT137 Image Editor")
         self.root.geometry("1000x600")
+        
+         self.main = tk.Frame(self.root)
+        self.main.pack(fill=tk.BOTH, expand=True)
+        
+        self.canvas_frame = tk.Frame(self.main, bg="black")
+        self.canvas_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.canvas = tk.Canvas(self.canvas_frame, bg="black")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+        self.control_panel = tk.Frame(self.main, width=220)
+        self.control_panel.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.status = tk.Label(
+            self.root,
+            text="No image loaded",
+            bd=1,
+            relief=tk.SUNKEN
+        )
+        self.status.pack(side=tk.BOTTOM, fill=tk.X)
+
 
