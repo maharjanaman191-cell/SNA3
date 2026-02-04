816d282d05e6c88ecd5307305f539c4d1d5b7b4a
rishichaulagain27-sys <rishi.chaulagain27@gmail.com>
2026-02-04 23:31:43 +0930

    adding final code in main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import tkinter as tk
 from controller import AppController
 from gui import ImageEditorGUI
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    controller = AppController()
+    app = ImageEditorGUI(root, controller)
+    root.mainloop()
+
