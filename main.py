028a4a9a74e16b4ba5e057041e8f05cb878917ce
rishichaulagain27-sys <rishi.chaulagain27@gmail.com>
2026-02-05 00:11:45 +0930

    adding comments on main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import tkinter as tk
 from controller import AppController
 from gui import ImageEditorGUI
 
+# Main entry point of the application
 if __name__ == "__main__":
+
+# Creating the main Tkinter window
     root = tk.Tk()
+
+# Creating the controller to handle image logic
     controller = AppController()
+    
+# Creating the GUI and passing the controller
     app = ImageEditorGUI(root, controller)
+
+# Starting the Tkinter event loop
     root.mainloop()
 
